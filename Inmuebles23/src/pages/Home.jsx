18037481178b516f7e07d5e2167d00812160051c
jesusavina-commits <jesus.avina@roii.tech>
2026-01-14 11:41:44 -0600

    feat(02): add tanstack, tanstack router

---
+++ Inmuebles23/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="page">
+      <h2>Home Page</h2>
+      <p>Welcome to Inmuebles23, your trusted real estate platform.</p>
+    </div>
+  )
+}
+
+export default Home
